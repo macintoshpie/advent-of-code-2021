12b1501f85b06ad0764b60e81b6418ef50eab6e7
Ted Summer <ted.summer2@gmail.com>
2021-12-12 17:32:41 -0700

    feat: day 12

---
+++ day_12_part_02.py
@@ -0,0 +1,114 @@
+from __future__ import annotations
+from typing import Dict
+
+from python_utils import readlines
+
+
+class Node:
+    twice_visited_small_node = None
+
+    def __init__(self, label_: str):
+        self.label = label_
+        self.is_large = label_.upper() == label_
+        self.neighbors: list[Node] = []
+        self.visited = False
+
+
+    def add_neighbor(self, node: Node) -> None:
+        self.neighbors.append(node)
+
+
+    @staticmethod
+    def parse_graph(adjacency_lines: list[str]) -> Dict[str, Node]:
+        nodes_by_label = {}
+        for line in adjacency_lines:
+            a_label, b_label = line.split('-')
+            a_node, b_node = nodes_by_label.get(a_label), nodes_by_label.get(b_label)
+            if not a_node:
+                a_node = Node(a_label)
+                nodes_by_label[a_label] = a_node
+            if not b_node:
+                b_node = Node(b_label)
+                nodes_by_label[b_label] = b_node
+
+            a_node.add_neighbor(b_node)
+            b_node.add_neighbor(a_node)
+        
+        return nodes_by_label
+
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Node):
+            return NotImplemented
+        return self.label == other.label
+
+
+    @property
+    def visitable(self):
+        if self.label == 'start':
+            return False
+
+        if self.is_large:
+            return True
+
+        if not self.visited:
+            return True
+        else:
+            return Node.twice_visited_small_node == None
+
+
+    def visit(self):
+        if self.is_large:
+            return
+
+        if not self.visited:
+            self.visited = True
+        else:
+            # visiting this node for the second time
+            assert Node.twice_visited_small_node == None
+            Node.twice_visited_small_node = self
+
+    def unvisit(self):
+        if self.is_large:
+            return
+
+        if self.visited and (Node.twice_visited_small_node == self):
+            Node.twice_visited_small_node = None
+        else:
+            self.visited = False
+
+
+    def get_paths(self, other: Node, paths_to_get_here: list[list[Node]]) -> list[list[Node]]:
+        if self == other:
+            return paths_to_get_here
+
+        paths_to_other: list[list[Node]] = []
+        for neighbor in self.neighbors:
+            if neighbor.visitable:
+                neighbor.visit()
+
+                paths_to_neighbor = [path + [neighbor] for path in paths_to_get_here]
+                paths_to_end = neighbor.get_paths(other, paths_to_neighbor)
+                for path in paths_to_end:
+                    if path and path[-1] == other:
+                        # successfully made it to the end
+                        paths_to_other.append(path)
+
+                neighbor.unvisit()
+
+        return paths_to_other
+
+
+def main():
+    input = [l.strip() for l in readlines()]
+
+    nodes_by_label = Node.parse_graph(input)
+    start_node = nodes_by_label['start']
+    end_node = nodes_by_label['end']
+
+    paths = start_node.get_paths(end_node, [[start_node]])
+    print(len(paths))
+
+
+if __name__ == '__main__':
+    main()
